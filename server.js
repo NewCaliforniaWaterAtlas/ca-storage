87dfa6376b4d901f553eb1fb9d0132bad991bfb5
videmsky <stuffaccounts@gmail.com>
2014-06-20 00:10:24 -0700

    tinkered in root

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('mongoose'),
+  passport = require('passport'),
+  request = require('request'),
+  logger = require('mean-logger');
+
+/**
+ * Main application entry file.
+ * Please note that the order of loading is important.
+ */
+
+// Initializing system variables
+var config = require('./server/config/config');
+// var db = mongoose.connect(config.db);
+
+// // Bootstrap Models, Dependencies, Routes and the app as an express app
+// var app = require('./server/config/system/bootstrap')(passport, db);
+
+// // Start the app by listening on <port>, optional hostname
+// app.listen(config.port, config.hostname);
+// console.log('Mean app started on port ' + config.port + ' (' + process.env.NODE_ENV + ')');
+
+// // Initializing logger
+// logger.init(app, passport, mongoose);
+
+// // Expose app
+// exports = module.exports = app;
+
+
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello AWSBOX\n');
+  res.end('Hello CA-Storage\n');
 }).listen(process.env['PORT'] || 8080, '127.0.0.1');
